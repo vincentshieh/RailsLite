96013e60d15549972449ec5a158fc7e4b7fe1059
Vincent Shieh <vincent.shieh@gmail.com>
2015-03-17 13:47:20 -0700

    finish phase II: Basic ControllerBase

--- lib/phase2/controller_base.rb
+++ lib/phase2/controller_base.rb
@@ -1,26 +1,35 @@
+require 'byebug'
+
 module Phase2
   class ControllerBase
     attr_reader :req, :res
 
     # Setup the controller
     def initialize(req, res)
-      @req, @res = req, res
+      @req, @res, @already_built_response = req, res, false
     end
 
     # Helper method to alias @already_built_response
     def already_built_response?
+      @already_built_response
     end
 
     # Set the response status code and header
     def redirect_to(url)
+      raise "already built response" if @already_built_response
+      @res.header['location'] = url
+      @res.status = 302
+      @already_built_response = true
     end
 
     # Populate the response with content.
     # Set the response's content type to the given type.
     # Raise an error if the developer tries to double render.
     def render_content(content, content_type)
+      raise "already built response" if @already_built_response
       @res.content_type = content_type
       @res.body = content
+      @already_built_response = true
     end
   end
 end
